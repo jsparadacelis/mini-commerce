7557c3a085521a96f88c70f35c7ae88e4ccf57de
jsparadacelis <parada.celis@gmail.com>
2018-11-24 19:24:50 -0500

    Item's and order's models

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+
+    order_id = models.IntegerField()
+    terminal_id = models.CharField(max_length=10)
+    total_amount = models.IntegerField()
--- products/models.py
+++ products/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from payment.models import Order
 
 class Product(models.Model):
 
     name = models.CharField(max_length=50)
     value = models.IntegerField()
     image = models.ImageField(upload_to='media/')
+
+class Item(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    cant = models.IntegerField()
+    total_amount = models.IntegerField()
