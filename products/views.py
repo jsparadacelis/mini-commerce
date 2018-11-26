960508407862d84bb40a21274456116073976ceb
jsparadacelis <parada.celis@gmail.com>
2018-11-26 15:44:47 -0500

    View confirm_pay

--- products/views.py
+++ products/views.py
@@ -4,10 +4,12 @@ from .models import Product
 # Create your views here.
 def list_products(request):
     products_list = Product.objects.all()
+    cant_products = Product.objects.count()
     return render(
         request,
         'products/index.html',
         {
-            "products_list" : products_list
+            "products_list" : products_list,
+            "num" : cant_products
         }
     )
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,16 @@
 let num_products = 0;
+let list_products = []
+$(document).ready(function() {
+    $('.btn').on('click', function (e) {
+        let id = $(this).data("value");  
+        let item = [
+             $( "input[name=name_product"+id+"]" ).val(),
+             $( "input[name=value_product"+id+"]" ).val()
+        ]
+        list_products.push(item)
+        $( "input[name=data]" ).val(list_products)
+    });  
 
-$(".btn_set_cant").click(
-    function(){
-        num_products++;
-        $("input[name = cant_product]").val(num_products);  
-    }
-);
+
+});
 
