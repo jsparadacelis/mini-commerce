f2e1a4e185bc0fb0d33c3df0efefe723ba579113
jsparadacelis <parada.celis@gmail.com>
2018-11-30 16:41:15 -0500

    Heroku deploy

--- payment/views.py
+++ payment/views.py
@@ -56,7 +56,7 @@ def pay_products(request):
                         arr_items, 
                         str(secrets.token_hex(6)), 
                         expired_date, 
-                        str(request.user.client.terminal), 
+                        "engativa", 
                         ip_addr
                 )
                 data_order_request = response
