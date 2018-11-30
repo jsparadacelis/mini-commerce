c8a1188b10b5ef073a6880c36fc92d3c411bde4d
jsparadacelis <parada.celis@gmail.com>
2018-11-30 16:51:00 -0500

    Style for reverted order

--- payment/views.py
+++ payment/views.py
@@ -56,7 +56,7 @@ def pay_products(request):
                         arr_items, 
                         str(secrets.token_hex(6)), 
                         expired_date, 
-                        "engativa", 
+                        "default", 
                         ip_addr
                 )
                 data_order_request = response
