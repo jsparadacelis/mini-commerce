7c3fdee90aca42f854ba7fd62cc55d3b2e40acfb
jsparadacelis <parada.celis@gmail.com>
2018-11-30 20:41:55 -0500

    Change templates var

--- mini_commerce/settings.py
+++ mini_commerce/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'mini_commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Before: 'DIRS': ['templates']
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
